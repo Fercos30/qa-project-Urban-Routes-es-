13fc46e94017982ec7b0c3e04e77a53841c938a3
Fercos30 <fernandaco30@gmail.com>
2024-08-25 15:55:58 -0600

    first commit

---
+++ data.py
@@ -0,0 +1,6 @@
+urban_routes_url = ('https://cnt-f630f8b9-c58a-4e0e-b98c-1d5c0bf17c63.containerhub.tripleten-services.com?lng=es' )
+address_from = 'East 2nd Street, 601'
+address_to = '1300 1st St'
+phone_number = '+1 123 123 12 12'
+card_number, card_code = '1234 5678 9100', '111'
+message_for_driver = 'Muéstrame el camino al museo'
